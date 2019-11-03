864dfdbb2e81f612ebb2e2e4a4805107d834d302
Graciela Ardiles <gardiles@baufest.com.ar>
2019-11-03 12:04:45 -0300

    UpdatedResponsive map

--- script/script.js
+++ script/script.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
-    });             
+    });
 });
