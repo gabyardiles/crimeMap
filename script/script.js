267f83f54ae89a9a264e119352e8cac3518bc3af
Graciela Ardiles <gardiles@baufest.com.ar>
2019-11-04 08:49:08 -0300

    Add validate date

--- script/script.js
+++ script/script.js
@@ -3,4 +3,23 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
     });
+
+
+    // validate Date
+    
+    function isValidDate(str) {
+        
+        var regex = /(\d{1,2})[-\/](\d{1,2})[-\/](\d{4})\s*(\d{0,2}):?(\d{0,2}):?(\d{0,2})/,
+        parts = regex.exec(str); 
+        
+        if (parts) {
+          var date = new Date ( (+parts[3]), (+parts[1])-1, (+parts[2]), (+parts[4]), (+parts[5]), (+parts[6]) );
+          if ( ( date.getDate() == parts[2] ) && ( date.getMonth() == parts[1]-1 ) && ( date.getFullYear() == parts[3] ) ) {
+            return date;
+          }
+        } 
+        
+        return false;
+      }
+    
 });
