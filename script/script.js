ddbe15e1c9d099c062439097606f873e2fbf4554
Graciela Ardiles <gardiles@baufest.com.ar>
2019-11-05 10:27:41 -0300

    Add validate date filter

--- script/script.js
+++ script/script.js
@@ -4,17 +4,43 @@ $(document).ready(function () {
         $(this).toggleClass('active');
     });
 
-    var date = $('#dateFilter')
-    var Currentdate = new Date();
-
-    var day = Currentdate.getDate();
-    var month = Currentdate.getMonth() + 1;
-    var year = Currentdate.getFullYear();
-
-    if (month < 10) month = "0" + month;
-    if (day < 10) day = "0" + day;
-
-    var today = year + "-" + month + "-" + day;       
-    date.val(today);
+    validateDate()
     
+    function validateDate() {
+        var date = $('#dateFilter')
+        var Currentdate = new Date();
+    
+        var day = Currentdate.getDate();
+        var month = Currentdate.getMonth() + 1;
+        var year = Currentdate.getFullYear();
+    
+        if (month < 10) month = "0" + month;
+        if (day < 10) day = "0" + day;
+    
+        var today = year + "-" + month + "-" + day;       
+        date.val(today);
+
+        // Validate range date
+        date.on('change', function() {
+            var regex = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+            var dateSelected = new Date(date.val())
+            var daySelected = dateSelected.getDate();
+            var monthSelected = dateSelected.getMonth() + 1;
+            var yearSelected = dateSelected.getFullYear(); 
+
+            if (monthSelected < 10) monthSelected = "0" + monthSelected;
+            if (daySelected < 10) daySelected= "0" + daySelected;
+    
+            var selectionDate = yearSelected + "-" + monthSelected + "-" + daySelected;
+
+            if (selectionDate >= today) {
+                alert("La fecha elegida no puede ser mayor al día de hoy");
+                date.val(today);
+            } else if (dateSelected.val() != '' && !dateSelected.val().match(regex)) {
+                alert("Formato de fecha invalido, por favor reintente");
+            }            
+            
+        });
+    
+    };
 });
