79b8098530e3db1478beb4a06370a67a7771437b
Graciela Ardiles <gardiles@baufest.com.ar>
2019-11-13 11:06:05 -0300

    Add types crime for map (query)

--- script/map.js
+++ script/map.js
@@ -6,38 +6,48 @@ $(document).ready(function () {
       url: "../php/mapa_delito.php",
       type:"GET"
       }).done(function(data) {
+        var myJsonString = JSON.parse(data);
+        console.log(myJsonString.features);
         // Map
         mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FicmllbGFhcmRpbGVzIiwiYSI6ImNrMndwM2h0MzBmNjgzbHF3OWtodXgzeTcifQ.J7akpCRoHz6s2Dl2W6gcJA';
-        var map = new mapboxgl.Map({
-          container: 'map',
-          style: 'mapbox://styles/mapbox/streets-v11',
-          center: [-58.392321, -34.737410],
-          zoom: 10
+      
+        myJsonString.features.forEach(function(marker) {
+          console.log(marker);
+          var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-58.392321, -34.737410],
+            zoom: 10
+          });
+          
+          map.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+          }));
+
+          map.on('load', function() {
+            map.addLayer({
+              "id": "points",
+              "type": "symbol",
+              "source": {
+              "type": "geojson",
+              "data": myJsonString
+              },
+              "layout": {
+              "icon-image": ["concat", ["get", "icon"], "-15"],
+              "text-field": ["get", "title"],
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 0.6],
+              "text-anchor": "top"
+              }
+            });
         });
         
-        map.addControl(new mapboxgl.GeolocateControl({
-          positionOptions: {
-              enableHighAccuracy: true
-          },
-          trackUserLocation: true
-        }));
-        map.on('load', function() {
-          map.addLayer({
-            "id": "points",
-            "type": "symbol",
-            "source": {
-            "type": "geojson",
-            "data": data
-            },
-            "layout": {
-            "icon-image": ["concat", ["get", "icon"], "-15"],
-            "text-field": ["get", "title"],
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-offset": [0, 0.6],
-            "text-anchor": "top"
-            }
-          });
-      });
+
+
+        });
   });
   };
 });
